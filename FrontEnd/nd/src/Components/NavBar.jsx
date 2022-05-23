004e081ab5213755437907383bf55f350b7c403c
Ajinkya Bhapkar <101281801+AjinkyaBhapkar@users.noreply.github.com>
2022-05-23 16:53:54 +0530

    app set up and cleaned of unnecessary items

---
+++ FrontEnd/nd/src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+const NavBar = () => {
+  return (
+    <div>
+      <MenuIcon/>
+    </div>
+  )
+}
+
+export default NavBar
